62cc793b0586878e8be750499a8f43a4774c20e3
Belabz <Belabz@abdelazizs-mbp.home>
2017-10-21 21:00:56 +0200

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-
 import AppRouter from './routers/AppRouter';
-
 import storeConfig from './redux/store/store-config';
-import { addExpense } from "./redux/actions/expenses";
+// import { addExpense } from "./redux/actions/expenses";
 // import { setTextFilter } from "./redux/actions/filters";
-import getVisibleExpenses from './redux/selectors/expenses';
-
+// import getVisibleExpenses from './redux/selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 /* -------------------------------------------------------------------------
    - Subscribe to any store changes
 ------------------------------------------------------------------------- */
 const store = storeConfig();
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 /* -------------------------------------------------------------------------
    - Add new expenses
 ------------------------------------------------------------------------- */
-store.dispatch(addExpense({ description: 'Water bill', note: 'Water bill note', amount: '5000', createdAt: 1100000 }));
-store.dispatch(addExpense({ description: 'Gas bill', note: 'Gas bill note', amount: '2000', createdAt: 1200000 }));
-store.dispatch(addExpense({ description: 'Rent', note: 'Rent bill note', amount: '222000', createdAt: 12000200 }));
+// store.dispatch(addExpense({ description: 'Water bill', note: 'Water bill note', amount: '5000', createdAt: 1100000 }));
+// store.dispatch(addExpense({ description: 'Gas bill', note: 'Gas bill note', amount: '2000', createdAt: 1200000 }));
+// store.dispatch(addExpense({ description: 'Rent', note: 'Rent bill note', amount: '222000', createdAt: 12000200 }));
 
 /* -------------------------------------------------------------------------
    - Set a filter for the expenses
@@ -42,7 +40,7 @@ store.dispatch(addExpense({ description: 'Rent', note: 'Rent bill note', amount:
    - The Provider will aloow us to provide the store to all the components
 ------------------------------------------------------------------------- */
 const jsx = (
-	<Provider store={store}>
+	<Provider store={ store }>
 		<AppRouter/>
 	</Provider>
 );
